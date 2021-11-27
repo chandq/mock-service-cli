a1bd549e24da958178a3b1244c4b2f472dfcc643
chendq <deqiaochen@foxmail.com>
2021-11-27 23:45:10 +0800

    feat: 支持除get请求之外的其他所有常用类型

--- mock/test.js
+++ mock/test.js
@@ -1,7 +1,25 @@
+/*
+ * @description:
+ * @Date: 2021-10-26 10:06:45
+ * @LastEditors: chendq
+ * @LastEditTime: 2021-11-27 23:39:42
+ * @Author: chendq
+ */
 const Mock = require('mockjs');
 
 module.exports = {
-  '/mock/:id/test': { aa: 1, bb: '西西小飞龙' },
+  '/mock/:id/test': { aa: 1, bb: '默认GET请求' },
+  'GET /mock/:id/test': { aa: 1, bb: '指定GET 方法' },
+  'POST /mock/:id/test': { aa: 1, bb: 'POST 方法' },
+  'DELETE /mock/:id/test': { aa: 1, bb: 'DELETE 方法' },
+  'PUT /mock/:id/test': { aa: 1, bb: 'PUT 方法' },
+  'PATCH /mock/:id/test': { aa: 1, bb: 'PATCH 方法' },
+  'HEAD /mock/:id/test': { aa: 1, bb: 'HEAD 方法' },
+  'OPTIONS /mock/:id/test': { aa: 1, bb: 'OPTIONS 方法' },
+  'COPY /mock/:id/test': { aa: 1, bb: 'COPY 方法' },
+  'LINK /mock/:id/test': { aa: 1, bb: 'LINK 方法' },
+  'UNLINK /mock/:id/test': { aa: 1, bb: 'UNLINK 方法' },
+  'PURGE /mock/:id/test': { aa: 1, bb: 'PURGE 方法' },
   '/mock/video/test': (req, res) => {
     res.json({ aa: 1, bb: 'asdf' });
   },
