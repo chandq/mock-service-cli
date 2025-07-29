9673032431e4f6a275f229544dda71958c542d63
chendq <deqiaochen@foxmail.com>
2025-07-29 17:19:21 +0800

    fix: support to set response headers for static server

--- mock/element-template.js
+++ mock/element-template.js
@@ -1,5 +1,16 @@
 const Mock = require('mockjs');
+
 module.exports = {
+  '/mock/api/randomList': (req, res) => {
+    res.json({
+      code: 0,
+      message: '成功',
+      data: Mock.mock({
+        text: Mock.Random.title(15),
+        'records|20': [{ name: Mock.Random.title(1, 2), age: Mock.Random.integer(1, 100) }]
+      })
+    });
+  },
   '/mock/api/getImage': {
     code: 0,
     message: '成功',
@@ -7,12 +18,12 @@ module.exports = {
       url: 'https://cdn.pixabay.com/photo/2021/01/28/21/12/wave-5959087__340.jpg'
     }
   },
-  '/mock/api/getRandomText': (req, res) => {
+  '/mock/api/randomText': (req, res) => {
     res.json({
       code: 0,
       message: '成功',
       data: {
-        text: Mock.mock('@ctitle')
+        text: Mock.Random.title(15)
       }
     });
   }
